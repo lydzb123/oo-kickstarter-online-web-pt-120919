5881fd9d636ba6b22fddbb9db704790728d97ad1
Lydia B <lydiabaik@gmail.com>
2020-01-27 08:54:31 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project (project_name)
+    @backed_projects << project_name
+    project_name.backers << self
+
+  end
+
+end
